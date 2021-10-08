1a7ed3b51f8d1e95098b85625a019412782c8d50
Ryan Logan <ryan_plogan@yahoo.com>
2021-10-08 12:47:37 -0700

    trying getting rid of hashrouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
-// import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import About from "./components/About";
 import Footer from "./components/Footer";
-import { HashRouter as Router } from "react-router-dom";
-import {BrowserRouter as Switch, Route} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ContactForm from "./components/ContactForm";
 
 const App = () => {
-  // const pages = ["about", "portfolio", "contact"];
-  // const [currentPage, setCurrentPage] = useState(pages[0]);
   return (
     <Router >
       <div className="App">
-        <Navbar
-          // pages={pages}
-          // currentPage={currentPage}
-          // setCurrentPage={setCurrentPage}
-        />
+        <Navbar/>
         <main>
           <Switch>
             <Route exact path="/">
